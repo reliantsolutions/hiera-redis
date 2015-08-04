fbd556cd52994beb81c74213b62e373739691655
Hugo Parente Lima <hugo@reliantsolutions.com>
2015-08-03 22:02:36 -0300

    Some basic tests, just to be sure the basic stuff is working.

--- spec/hiera_redis_spec.rb
+++ spec/hiera_redis_spec.rb
@@ -6,6 +6,7 @@ describe 'hiera-redis' do
     Hiera.new(config: {
                 default: nil,
                 backends: ['redis'],
+                hierarchy: %w(main common),
                 scope: {},
                 key: nil,
                 verbose: false,
@@ -14,8 +15,40 @@ describe 'hiera-redis' do
   end
   let(:redis) { Redis.new }
 
-  it 'works' do
+  before(:each) do
+    redis.flushdb
+  end
+
+  it 'works with a string key' do
     redis.set('common:foo', 'bar')
     expect(hiera.lookup('foo', '', '')).to eq('bar')
+  end
+
+  it 'works with a hash key' do
+    redis.hset('common:foo', 'ckey', 'cvalue')
+    redis.hset('main:foo', 'key', 'value')
+    expect(hiera.lookup('foo', '', '')).to eq('key' => 'value')
+    expect(hiera.lookup('foo', '', '', '', :hash)).to eq('key' => 'value', 'ckey' => 'cvalue')
+    expect(hiera.lookup('foo', '', '', '', {})).to eq('key' => 'value', 'ckey' => 'cvalue')
+  end
+
+  it 'works with list keys' do
+    redis.rpush('common:foo', 'value')
+    redis.rpush('common:foo', 'value2')
+    expect(hiera.lookup('foo', '', '')).to eq(%w(value value2))
+  end
+
+  it 'works with set keys' do
+    redis.sadd('common:foo', 'value')
+    expect(hiera.lookup('foo', '', '')).to eq(%w(value))
+  end
+
+  it 'obey the hierarchy' do
+    redis.set('common:foo', 'common')
+    redis.set('main:foo', 'main')
+
+    expect(hiera.lookup('foo', '', '')).to eq('main')
+    # now as an array, order is important.
+    expect(hiera.lookup('foo', '', '', '', :array)).to eq(%w(main common))
   end
 end
